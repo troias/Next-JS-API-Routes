abaa0f936ad23534fe8969ab7011670385a5fb4e
troy <hunter007nz@gmail.com>
2021-10-13 22:50:25 +1300

    fixed api

---
+++ pages/api/feedback.js
@@ -0,0 +1,65 @@
+import fs from "fs";
+import path from "path";
+
+
+export const buildFeedBackPath = () => {
+    return  path.join(process.cwd(), "data", "feedback.json");
+}
+
+const extractFeedBackData = () => {
+    const filePath = buildFeedBackPath()
+    const fileData = readFileData(filePath)
+    return fileData
+}
+
+const readFileData = (filePath) => {
+    return  fs.readFileSync(filePath)
+}
+
+const jsonToObject = (fileData) => {
+    return JSON.parse(fileData);
+}
+
+
+const handler = (req, res) => {
+
+    if (req.method === "POST") {
+
+        const parsedFeedbackData = JSON.parse(req.body)
+        const email = parsedFeedbackData.email;
+        const feedback = parsedFeedbackData.feedback;
+
+        const newFeedback = {
+            id: new Date().toISOString(),
+            email: email,
+            feedback: feedback,
+        };
+        // console.log("requestBody", req.body)
+        // console.log("newFeedback", newFeedback);
+        // console.log("parsedFeedbackData", parsedFeedbackData)
+
+        const filePath = buildFeedBackPath()
+        const fileData = readFileData(filePath)
+        const data = jsonToObject(fileData);
+        data.push(newFeedback);
+        fs.writeFileSync(filePath, JSON.stringify(data));
+
+        res.status(201).json({
+            message: "Sucess",
+            feedback: newFeedback,
+        });
+    } else {
+        // const feedbacks = extractFeedBackData()
+        // res.json(feedbacks)
+    }
+
+    if (req.method === "GET") {
+        const feedback = extractFeedBackData()
+        console.log("feedback", feedback)
+        res.json(feedback)
+    }
+
+    return res;
+};
+
+export default handler;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,89 @@
-function HomePage() {
+
+import { useRef, useState } from "react";
+
+function HomePage(props) {
+  const { feedback } = props;
+
+  console.log("homepage props", props)
+  const [showFB, setShowFB] = useState(false);
+  const [loading, setLoading] = useState(false)
+
+  const emailRef = useRef();
+  const feedbackRef = useRef();
+
+  const submitFeedbackHandler = async (event) => {
+    event.preventDefault();
+    const currEmailVal = emailRef.current.value;
+    const currFeedbackVal = feedbackRef.current.value;
+
+    const item = {
+      email: currEmailVal,
+      feedback: currFeedbackVal,
+    };
+
+    const req = await fetch("/api/feedback", {
+      method: "POST",
+      body: JSON.stringify(item),
+    });
+    const res = await req.json();
+    console.log("feedbackResponse", res)
+  };
+
+  const showFeedbackHandler = async () => {
+    setShowFB(prevState => !prevState);
+  };
+
+  if (loading) {
+    return (
+      <div>
+        <p>Loading</p>
+      </div>
+    )
+  }
+
+ 
   return (
     <div>
       <h1>The Home Page</h1>
+      <form onSubmit={submitFeedbackHandler}>
+        <div>
+          <label htmlFor="Email">Your email address</label>
+          <input type="email" id="email" ref={emailRef} />
+        </div>
+        <div>
+          <label htmlFor="feedback">Your feedback</label>
+          <textarea type="text" id="feedback" rows="5" ref={feedbackRef} />
+        </div>
+        <button>Send Feedback</button>
+      </form>
+      <button onClick={showFeedbackHandler}>Load Feedback</button>
+      {showFB &&
+        feedback.map((feedback) => (
+          <ul key={feedback.id}>
+            <li>{feedback.email}</li>
+            <li>{feedback.feedback}</li>
+          </ul>
+        ))}
+     
     </div>
   );
 }
+
+export const getStaticProps = async () => {
+
+  const req = await fetch('http://localhost:3000/api/feedback');
+
+  if (!req.ok) {
+    throw new Error("fetch failed")
+  }
+
+  const res = await req.json();
+  // console.log("GSP", res)
+  return {
+    props: {
+      feedback: res,
+    }, revalidate: 2
+  };
+};
 
 export default HomePage;
